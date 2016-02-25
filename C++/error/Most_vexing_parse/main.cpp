f752c5148ae06ec6b9babdb3d0f68db7bf603fae
Yue Gan <gany.fml@gmail.com>
2016-02-25 15:45:13 -0600

    Most vexing parse

---
+++ C++/error/Most_vexing_parse/main.cpp
@@ -0,0 +1,16 @@
+class Timer {
+  public:
+    Timer();
+};
+
+class TimeKeeper {
+  public:
+    TimeKeeper(const Timer& t);
+
+    int get_time();
+};
+
+int main() {
+  TimeKeeper time_keeper(Timer());
+  return time_keeper.get_time();
+}
