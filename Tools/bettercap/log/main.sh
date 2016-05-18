39691772f6d5a4f149d893d0f175f13cbf5b45d5
Yue Gan <gany.fml@gmail.com>
2016-05-17 20:44:40 -0500

    bettercap added

---
+++ Tools/bettercap/log/main.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#vim: set noexpandtab tabstop=2:
+
+#Output content to local file
+sudo bettercap --log out.log
--- programming_document/python/template/main.py
+++ programming_document/python/template/main.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
 # vim: set noexpandtab tabstop=2 shiftwidth=2 softtabstop=-1 fileencoding=utf-8:
 
+#Used for Ctrl-C
+import sys
+#http://stackoverflow.com/questions/7073268/remove-traceback-in-python-on-ctrl-c
+signal.signal(signal.SIGINT, lambda x,y: sys.exit(128 + signal.SIGINT))
+
+#Used for pip
+#Ignore SIG_PIPE and don't throw exceptions on it... (http://docs.python.org/library/signal.html)
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)
