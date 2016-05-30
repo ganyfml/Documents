cf3b673ea31916b368ef66e6e504c64af5911ccf
Yue Gan <gany.fml@gmail.com>
2016-05-30 15:34:42 -0500

    python template update

--- programming_document/python/template/main.py
+++ programming_document/python/template/main.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # vim: set noexpandtab tabstop=2 shiftwidth=2 softtabstop=-1 fileencoding=utf-8:
 
+#Used for Ctrl-C
+import sys
+import signal
+#http://stackoverflow.com/questions/7073268/remove-traceback-in-python-on-ctrl-c
+signal.signal(signal.SIGINT, lambda x,y: sys.exit(128 + signal.SIGINT))
+
+#Used for pip
+#Ignore SIG_PIPE and don't throw exceptions on it... (http://docs.python.org/library/signal.html)
+signal.signal(signal.SIGPIPE, signal.SIG_DFL)
