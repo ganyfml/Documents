ad04bd8abcde9e0948144947c078830adfb76034
Yue Gan <gany.fml@gmail.com>
2016-05-31 16:00:34 -0500

    new MWE added

--- programming_document/python/syntax/comprehension/dict/main.py
+++ programming_document/python/syntax/comprehension/dict/main.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 # vim: set noexpandtab tabstop=2 shiftwidth=2 softtabstop=-1 fileencoding=utf-8:
 
-a = [3, 4, 5, 6]
-
-b = filter(lambda x: x > 4, a)
-
-print b
+d = {n: n**2 for n in range(5)}
+print d
---
+++ programming_document/python/syntax/comprehension/list/main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set noexpandtab tabstop=2 shiftwidth=2 softtabstop=-1 fileencoding=utf-8:
+
+a = [1, 2, 3, 4]
+b = [ x + 1 for x in a]
+print b
+
+a = [1, 2, 3, 4]
+b = [2, 3, 4, 6]
+c = [[x, y] for x in a for y in b if x + y == 7]
+print c
+d = [[x, y] for x in a for y in b]
+print d
+
+e = (x + 1 for x in range(5))
+print e
+print list(e)
