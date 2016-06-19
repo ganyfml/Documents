d203ea03f176274c7a6e81763f2a7067e70e1d35
Yue Gan <gany.fml@gmail.com>
2016-06-19 17:15:09 -0500

    python MWE: sys.{exit,stderr}, try added

---
+++ programming_document/python/MWE/default/try/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set noexpandtab tabstop=2 shiftwidth=2 softtabstop=-1 fileencoding=utf-8:
+
+try:
+	print 1/0
+except Exception as e:
+	print e
+	pass
---
+++ programming_document/python/MWE/sys/stderr/main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set noexpandtab tabstop=2 shiftwidth=2 softtabstop=-1 fileencoding=utf-8:
+
+import sys
+sys.stderr.write("Error!\n")
