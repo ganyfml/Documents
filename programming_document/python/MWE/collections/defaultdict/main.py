5b7d2bb8217c105f8a2bf095eef24b34a7ff6655
Yue Gan <gany.fml@gmail.com>
2016-07-17 17:39:55 -0500

    default dict added

---
+++ programming_document/python/MWE/collections/defaultdict/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set noexpandtab tabstop=2 shiftwidth=2 softtabstop=-1 fileencoding=utf-8:
+
+from collections import defaultdict
+
+test = defaultdict(list)
+print test['hello_world!']
