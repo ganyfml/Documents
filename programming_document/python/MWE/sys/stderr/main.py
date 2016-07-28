e15c7fb9098c4c9ba2a09a6934d1656c1f4fb0dc
Yue Gan <gany.fml@gmail.com>
2016-07-28 09:09:57 -0500

    clean up the repo

--- programming_document/python/MWE/sys/stderr/main.py
+++ programming_document/python/MWE/sys/stderr/main.py
@@ -3,4 +3,10 @@
 # vim: set noexpandtab tabstop=2 shiftwidth=2 softtabstop=-1 fileencoding=utf-8:
 
 import sys
+
 sys.stderr.write("Error!\n")
+
+log = sys.stderr.write
+log('Error!\n')
+
+print >> sys.stderr, 'Error!\n'
--- programming_document/python/template/main.py
+++ programming_document/python/template/main.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 # vim: set noexpandtab tabstop=2 shiftwidth=2 softtabstop=-1 fileencoding=utf-8:
 
 #Used for Ctrl-C
